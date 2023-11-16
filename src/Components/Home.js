4b5be85958f358fc6cd07262f39b448603e373ea
Md Sheik <mdsheik633@hotmail.com>
2023-11-16 02:13:31 -0500

    Need to finish Home and projests ASAP.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <main className="contact-content">
+      <p> I do think this page needs to get build fsat. This needs to be done before the other pages.</p>
       <p>This will have some simple overview of some of the big things that I have worked on and that I am part of.</p>
       <p>For example: This is a bit like my resume, but it will have more details, and I want to make it look pretty.</p>
       <p>Those will have links to their own pages as well. Projects will bring them into projects, and </p>
